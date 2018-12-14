f9ad9342eec2ab183c6cd1ab1e8f1908b1160db2
Twometer <twometer@outlook.de>
2018-12-14 09:46:10 +0100

    Added property serializer

--- src/de/twometer/essentials/serializer/PropertiesSerializer.java
+++ src/de/twometer/essentials/serializer/PropertiesSerializer.java
@@ -10,16 +10,31 @@ import java.util.Properties;
 
 public class PropertiesSerializer {
 
+    public static Properties serialize(Object object) {
+        Properties properties = new Properties();
+        Class<?> objectClass = object.getClass();
+        for (Field field : objectClass.getDeclaredFields()) {
+            String propertyName = getPropertyName(field);
+            if (propertyName == null)
+                continue;
+            try {
+                properties.setProperty(propertyName, field.get(object).toString());
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return properties;
+    }
+
     public static <T> T deserialize(InputStream inputStream, Class<T> objectClass) throws IOException {
         Properties properties = new Properties();
         properties.load(inputStream);
         try {
             T object = objectClass.newInstance();
             for (Field field : objectClass.getDeclaredFields()) {
-                Property property = field.getAnnotation(Property.class);
-                if (property == null)
+                String propertyName = getPropertyName(field);
+                if (propertyName == null)
                     continue;
-                String propertyName = TextUtils.isNullOrEmpty(property.value()) ? field.getName() : property.value();
                 field.set(object, properties.getProperty(propertyName));
             }
             return object;
@@ -31,6 +46,13 @@ public class PropertiesSerializer {
 
     public static <T> T deserialize(String path, Class<T> objectClass) throws IOException {
         return deserialize(new FileInputStream(path), objectClass);
+    }
+
+    private static String getPropertyName(Field field) {
+        Property property = field.getAnnotation(Property.class);
+        if (property == null)
+            return null;
+        return TextUtils.isNullOrEmpty(property.value()) ? field.getName() : property.value();
     }
 
 }
